6827af190a0b20fab4fbc44e54354defa2f41a32
Abongile21 <abongiletheo00@gmail.com>
2024-06-28 14:04:15 +0200

    fixed scale unto the picture

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
 
 }
