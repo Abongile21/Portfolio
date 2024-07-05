1e5a2aa04f32b2b94bb085347039f1a1a885ee39
Abongile21 <abongiletheo00@gmail.com>
2024-07-05 13:20:31 +0200

    lowervolume on audio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -12,7 +12,9 @@ export class PortfolioComponent implements OnInit {
   private activeLink: HTMLElement | null = null;
 
   constructor(private router: Router) {
+    
     this.audio = new Audio('assets/sound1.mp3');
+    this.audio.volume = 0.01
   }
 
   ngOnInit() {
