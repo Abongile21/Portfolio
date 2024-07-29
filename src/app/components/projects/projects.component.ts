4d5936bb3fa80fb1e1e1ce5319207565d02dfa25
Abongile21 <abongiletheo00@gmail.com>
2024-07-29 10:29:47 +0200

    added resume-view component

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+  projects: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('https://vercel.com/abongiles-projects')
+      .subscribe((data) => {
+        this.projects = data;
+      });
+  }
 }
