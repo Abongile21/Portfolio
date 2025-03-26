787e7fb1d7b0929e4c4e391cda6ade97456725ce
Abongile21 <abongiletheo00@gmail.com>
2025-03-27 00:19:04 +0200

    adding again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { GoogleAnalyticsService } from 'ngx-google-analytics';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   darkMode: boolean = false;
 
+  constructor(
+    private router: Router,
+    private gaService: GoogleAnalyticsService
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.gaService.pageView(event.urlAfterRedirects);
+      }
+    });
+  }
   toggleDarkMode() {
     this.darkMode = !this.darkMode;
   }
