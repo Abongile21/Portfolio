b455e6ebaec51776feb65a41844275e1b0e4a95c
Abongile Nyakatya <65989651+Abongile21@users.noreply.github.com>
2026-06-13 15:38:51 +0200

    Update resumedata.service.ts

--- src/app/Services/resumedata.service.ts
+++ src/app/Services/resumedata.service.ts
@@ -34,9 +34,8 @@ export class ResumeDataService {
 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Blazor.png',
       'https://upload.wikimedia.org/wikipedia/commons/7/7d/Microsoft_.NET_logo.svg',
       'https://upload.wikimedia.org/wikipedia/commons/b/bd/Logo_C_sharp.svg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original',
-      'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original',
+      'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original'
 
-'https://share.google/j1XBFBnEGACJaZdrb'
       
     ],
     experience: [
