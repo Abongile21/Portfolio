fe855eb9504ef304c0bfc68d8fd9b7bc809be70d
Abongile21 <abongiletheo00@gmail.com>
2026-09-18 10:24:56 +0200

    Add Roker Eats project to resume data for enhanced portfolio representation

--- src/app/Services/resumedata.service.ts
+++ src/app/Services/resumedata.service.ts
@@ -75,6 +75,14 @@ export class ResumeDataService {
         link: 'https://zamikhaya.vercel.app/',
         technologies: ['MongoDB', 'ExpressJS', 'Angular', 'Tailwind', 'NodeJS','AWS-S3']
       },
+            {
+        image: '../../assets/roker-eats.png',
+        title: 'Roker Eats',
+        github: 'https://github.com/Abongile21/roker-eats',
+        description: 'Roker Eats is a restaurant landing page that showcases food items.',
+        link: 'https://roker-eats.vercel.app/roker-eats.html',
+        technologies: ['HTML', 'JavaScript', 'CSS', 'Tailwind']
+      },
       {
         image: '../../assets/Gush.png',
         title: 'GushLife Rides',
@@ -83,14 +91,7 @@ export class ResumeDataService {
         link: 'https://gushliferides.netlify.app/',
         technologies: ['HTML', 'JavaScript', 'CSS', 'Tailwind','Formspree']
       },
-      {
-        image: '../../assets/roker-eats.png',
-        title: 'Roker Eats',
-        github: 'https://github.com/Abongile21/roker-eats',
-        description: 'Roker Eats is a restaurant landing page that showcases food items.',
-        link: 'https://roker-eats.vercel.app/roker-eats.html',
-        technologies: ['HTML', 'JavaScript', 'CSS', 'Tailwind']
-      }
+
     ]
   };
 
